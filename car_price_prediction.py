ae754ab081b64277b48f946a40167f89cd1095c0
Ansarali156 <shaikansarali427@gmail.com>
2026-02-04 11:16:20 +0530

    added new feature Age

--- car_price_prediction.py
+++ car_price_prediction.py
@@ -36,13 +36,14 @@ for feature in features:
     le = LabelEncoder()
     df[feature] = le.fit_transform(df[feature].str.lower())
     encoders[feature] = le
-
-X = df[["name", "year", "fuel", "seller_type", "km_driven", "transmission", "owner"]]
+df["age"] = 2026 - df["year"]
+X = df[["name", "age", "fuel", "seller_type", "km_driven", "transmission", "owner"]]
 y = df[["selling_price"]]
 
 model = linear_model.LinearRegression()
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model.fit(x_train, y_train)
+r2 = r2_score(y_test,model.predict(x_test))
 
 @app.post("/predict")
 def predict(data: CarData):
@@ -50,7 +51,7 @@ def predict(data: CarData):
         # Encode categorical features
         input_dict = {
             "name": encoders["name"].transform([data.name.lower()])[0],
-            "year": data.year,
+            "age" : 2026 - data.year,
             "fuel": encoders["fuel"].transform([data.fuel.lower()])[0],
             "seller_type": encoders["seller_type"].transform([data.seller_type.lower()])[0],
             "km_driven": data.km_driven,
@@ -60,7 +61,8 @@ def predict(data: CarData):
 
         input_df = pd.DataFrame([input_dict])
         prediction = model.predict(input_df)[0][0]  # scalar value
-        return {"price": float(prediction)}
+        return {"price": float(prediction),
+                "r2" : float(r2)}
 
     except Exception as e:
         return {"error": str(e)}
